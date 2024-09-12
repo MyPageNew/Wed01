db1901442eb2a12a37485e2204e32b829b67ea01
davidmeloyan99 <davidmeloyan99@gmail.com>
2024-09-12 13:01:14 +0400

    Ok

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
+
 
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
@@ -15,15 +17,16 @@ var x = setInterval(function() {
     
     var distance = countDownDate - now;
     
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>օր</div>" + "<div class='hours block'>" + hours + "<br>ժամ</div>" + "<div class='minutes block'>" + minutes + "<br>րոպե</div>" + "<div class='seconds block'>" + seconds + "<br>վայրկյան</div></div>";
     
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
     }
 }, 1000);
+
